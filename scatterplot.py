27f6bf8a0199b0b85c416ba8355d96ccd1e76668
DrealVeerNanda <68409762+DrealVeerNanda@users.noreply.github.com>
2021-06-29 15:21:03 -0700

    Add files via upload

---
+++ scatterplot.py
@@ -0,0 +1,6 @@
+import plotly.express as px;
+import pandas as pd;
+
+df = pd.read_csv("Copy+of+data+-+data.csv")
+fig = px.scatter(df, x = "date", y = "cases", color = "country", size_max = 50)
+fig.show()
